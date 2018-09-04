65fe5f7794e45068e970c3d4604e83f887e0df80
leetou <leetou@fnguide.com>
2018-09-04 18:12:02 +0900

    add index, form

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 // Setup
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/kyu-post');
+const Post = mongoose.model('Post', { body: String });
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 
 // Routes
 app.get('/', (req, res) => {
-  console.log('in index route');
-  res.render('index');
+  const query = req.query;
+  console.log('query', query);
+  Post.find({}, (err, results) => {
+    res.render('index', { results: results });
+  });
+});
+
+app.post('/addpost', (req, res) => {
+  const post = new Post(req.body);
+  post
+    .save()
+    .then(aa => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(`Error in save`);
+    });
 });
 
 // Listening
