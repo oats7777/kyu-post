4d26802d073a29b7345393cdc8ae3f283a355873
leetou <leetou@fnguide.com>
2018-09-04 14:06:07 +0900

    add index page

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
+// Routes
+app.get('/', (req, res) => {
+  console.log('in index route');
+  res.render('index');
+});
+
 // Listening
 app.listen(3000, () => {
   console.log('Server listening on 3000');
